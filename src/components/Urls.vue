95040f636480b8593a889741d0cdbfca1cc7bc1e
Takuro Omori (orukt) <orukt@pixiv.co.jp>
2025-01-05 23:57:10 +0900

    port daily link generate logic

---
+++ src/components/Urls.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const today = new Date();
+const year = today.getFullYear().toString();
+const month = (today.getMonth() + 1).toString().padStart(2, "0");
+const day = today.getDate().toString().padStart(2, "0");
+
+const baseUrl = "https://scrapbox.io/dorary/";
+const subUrl = year + month + day;
+const bodyString = "?body=" + encodeURIComponent(`#${year}年${month}月`);
+
+const fullUrl = baseUrl + subUrl + bodyString;
+</script>
+
+<template>
+<div>
+  <a :href="fullUrl">link</a>
+</div>
+</template>
+
+<style scoped>
+</style>
